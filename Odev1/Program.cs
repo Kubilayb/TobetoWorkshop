f79990bef5f8961ca39ab4a861e32b2216170889
Kubilay <bkubilay9@gmail.com>
2024-03-06 17:21:47 +0300

    İlk sorunun cevabı yapıldı.

--- Odev1/Program.cs
+++ Odev1/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+//Odevler ve Cevapları
+
+//Odev 1:Switch case ile müşteriden alınan sipariş numarasına göre hangi ürünün sipariş edildiğini belirleyen algoritma yazınız.Input için Scanner kullanınız
+//Cevap1
+Console.WriteLine("Görüntülemek istediğinizin sipariş sepetinizin numarasını giriniz: ");
+int orderNumber = Convert.ToInt32(Console.ReadLine());
+switch (orderNumber)
+{
+    case 1:
+        Console.WriteLine("1 Numaralı Sipariş Sepetiniz.");
+        break;
+    case 2:
+        Console.WriteLine("2 Numaralı Sipariş Sepetiniz.");
+        break;
+    case 3:
+        Console.WriteLine("3 Numaralı Sipariş Sepetiniz.");
+        break;
+    case 4:
+        Console.WriteLine("4 Numaralı Sipariş Sepetiniz.");
+        break;
+    case 5:
+        Console.WriteLine("5 Numaralı Sipariş Sepetiniz.");
+        break;
+    default:
+        Console.WriteLine("Yanlış Numara Girdiniz!");
+        break;
+}
+
