7589d11322834bac42bb6dbd68a6ebc0a8b696d8
Kubilayb <109307078+Kubilayb@users.noreply.github.com>
2024-03-19 07:44:46 +0300

    Abstract class ile interface arasındaki farkı araştırma ödevi hariç tamamlandı.

---
+++ Odev6_OOP2/OOP2_Example2/1_Polymorphism/ClothingItems.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace OOP2_Examples._1_Polymorphism;
+
+
+//Abstract Ana sınıf: Giysi
+public abstract class ClothingItems
+{
+    public abstract void DisplayInfo();
+    public void Clotes()
+    {
+        Console.WriteLine("Giysiler Sergileniyor");
+    }
+}
+// Alt sınıf: Tişört
+public class TShirt : ClothingItems
+{
+    public override void DisplayInfo()
+    {
+        Console.WriteLine("Tişörtler");
+    }
+}
+
+// Alt sınıf: Pantolon
+public class Pants : ClothingItems
+{
+    public override void DisplayInfo()
+    {
+        Console.WriteLine("Pantolonlar.");
+    }
+}
+
+
+/*
+ 
+Bu kodda, ClothingItems adında soyut bir sınıf tanımlanmıştır. 
+Bu sınıf, DisplayInfo adında bir soyut metot içerir ve ayrıca Clothes adında bir somut metot bulunur.
+
+TShirt ve Pants sınıfları, ClothingItems sınıfından türetilmiştir. 
+Her iki sınıf da soyut metodu (DisplayInfo) uygular ve kendi giysi bilgilerini yazdırır.
+
+Bu yapı, polymorphism kullanarak farklı giysi tiplerini temsil eder. 
+Abstract class kullanılması, giysi tiplerinin ortak davranışlarını tanımlamak için 
+uygun bir yol sunar ve her bir giysi türünün özelliklerine göre davranmasını sağlar.
+ */
+
+public class Meyveler
+{
+    public virtual void DescriptionLog()
+    {
+        Console.WriteLine("Aciklama yazdirma base'i.");
+    }
+}
+
+public class Elma : Meyveler
+{
+    public override void DescriptionLog()
+    {
+        Console.WriteLine("Elma Elma Elma");
+    }
+}
+
+public class Kivi : Meyveler
+{
+    public override void DescriptionLog()
+    {
+        base.DescriptionLog();
+        Console.WriteLine("Kivi Kivi Kivi");
+    }
+}
+
+
+/*
+ virtual ve override anahtar kelimeleri, .NET platformunda polymorphism'i sağlayan bir mekanizmadır.
+
+virtual: Bu anahtar kelime, bir metodu (DescriptionLog metodu gibi) türetilmiş sınıfların üzerine yazılabilir hale getirir. 
+Yani, temel sınıfta tanımlanan bir metodu türetilmiş sınıflar istedikleri takdirde değiştirebilirler. 
+Ancak, türetilmiş sınıflar bu metodu değiştirmek zorunda değildir, yani temel sınıfın uygulamasını kullanmaya devam edebilirler. 
+virtual anahtar kelimesi, temel sınıfta tanımlanan bir metodu değiştirilebilir hale getirir ve türetilmiş sınıfların bu metodun davranışını değiştirmesine izin verir.
+
+override: Bu anahtar kelime, bir metodu türetilmiş bir sınıfta üzerine yazmak için kullanılır. 
+Bir metodu override etmek, temel sınıfta tanımlı olan orijinal uygulamanın yerine, türetilmiş sınıfın kendi uygulamasını kullanmasını sağlar. 
+Yani, override anahtar kelimesi, temel sınıfta virtual anahtar kelimesi ile işaretlenmiş bir metodu değiştirmek için kullanılır.
+
+Yukarıdaki kodda, Meyveler sınıfında DescriptionLog metodu virtual olarak işaretlenmiştir. 
+Bu, Elma ve Kivi sınıflarında bu metodu gerektiğinde değiştirebileceğimiz anlamına gelir. 
+Elma sınıfında, DescriptionLog metodu override edilmiş ve kendi özel uygulaması ile değiştirilmiştir. 
+Kivi sınıfında ise, DescriptionLog metodu hem temel sınıfın uygulamasını kullanmak için base.DescriptionLog() çağrısı yapılmış hem de kendi özel uygulaması eklenmiştir. 
+Bu sayede, Kivi sınıfı, hem temel sınıfın davranışını hem de kendi özel davranışını birleştirebilir.
+ */
--- Odev6_OOP2/OOP2_Example2/Program.cs
+++ Odev6_OOP2/OOP2_Example2/Program.cs
@@ -1,12 +1,47 @@
 ﻿
 
-
-
-
-
+using OOP2_Examples._1_Polymorphism;
 using OOP2_Examples._2_Inheritance;
+using OOP2_Examples._3_GenericType;
+using OOP2_Examples._5_Encapsulation;
+using static OOP2_Examples._1_Polymorphism.Pants;
 
 
+
+
+PolymorphismWithAbstractClass();
+PolymorphismWithVirtualOverride();
+Inheritance();
+GenericTypeExample();
+EncapsulationExample();
+KapsullemeOrnegi();
+
+
+Console.WriteLine("--------------Polymorphism ve Abstract Class Örneği--------------");
+static void PolymorphismWithAbstractClass()
+{
+    TShirt tshirt = new TShirt();
+    Pants pants = new Pants();
+
+    tshirt.DisplayInfo();
+    pants.DisplayInfo();
+
+    tshirt.Clotes();
+    pants.Clotes();
+  
+}
+
+Console.WriteLine("--------------Polymorphism ve Virtual Override Örneği--------------");
+
+static void PolymorphismWithVirtualOverride()
+{
+    Elma elma = new Elma();
+    Kivi kivi = new Kivi();
+
+    elma.DescriptionLog();
+    kivi.DescriptionLog();
+
+}
 
 
 Console.WriteLine("--------------Inheritance Örneği--------------");
@@ -18,60 +53,105 @@ Console.WriteLine("--------------Inheritance Örneği--------------");
  Türetilen sınıf, miras aldığı sınıfın tüm özelliklerini ve davranışlarını (alanlar, özellikler, metotlar) miras alır.
  Temel sınıf, miras veren sınıf olarak adlandırılırken, türetilen sınıf ise alt sınıf veya alt sınıf olarak adlandırılır.
 */
-
-Television television1 = new Television();
-television1.Id = 1;
-television1.Name = "Vestel";
-television1.Description = "Smart TV";
-television1.ModelYear = 2023;
-television1.Price = 20.000;
-television1.ResolutionQuality = " 4K QLED";
-television1.TVSize = "55 inches";
-
-Television television2 = new Television();
-television2.Id = 2;
-television2.Name = "Samsung";
-television2.Description = "Android TV";
-television2.ModelYear = 2022;
-television2.Price = 19.000;
-television2.ResolutionQuality = " 4K OLED";
-television2.TVSize = "50 inches";
-
-WashingMachine washingMachine1 = new WashingMachine();
-washingMachine1.Id = 3;
-washingMachine1.Name = "Bosch";
-washingMachine1.Description = "Yabancı Çamaşır Makinesi";
-washingMachine1.ModelYear = 2022;
-washingMachine1.Price = 8000;
-washingMachine1.MaxKg = "9 KG";
-washingMachine1.SpinSpeedRpm = 1400;
-
-WashingMachine washingMachine2 = new WashingMachine();
-washingMachine2.Id = 4;
-washingMachine2.Name = "Arçelik";
-washingMachine2.Description = "Yerli Çamaşır Makinesi";
-washingMachine2.ModelYear = 2019;
-washingMachine2.Price = 6500;
-washingMachine2.MaxKg = "8 KG";
-washingMachine2.SpinSpeedRpm = 1200;
-
-Television[] televisions = { television1, television2 };
-
-WashingMachine[] washingMachines = {washingMachine1, washingMachine2 };
-
-for (int i = 0; i < televisions.Length; i++)
+static void Inheritance()
 {
-    Console.WriteLine(televisions[i].Name + " " + televisions[i].Description + " " + televisions[i].ResolutionQuality);
-    
-}
 
-for (int i = 0;i < washingMachines.Length;i++)
-{
-    Console.WriteLine(washingMachines[i].Name + " " + washingMachines[i].MaxKg + " " + washingMachines[i].SpinSpeedRpm);
-}
+    Television television1 = new Television();
+    television1.Id = 1;
+    television1.Name = "Vestel";
+    television1.Description = "Smart TV";
+    television1.ModelYear = 2023;
+    television1.Price = 20.000;
+    television1.ResolutionQuality = " 4K QLED";
+    television1.TVSize = "55 inches";
 
+    Television television2 = new Television();
+    television2.Id = 2;
+    television2.Name = "Samsung";
+    television2.Description = "Android TV";
+    television2.ModelYear = 2022;
+    television2.Price = 19.000;
+    television2.ResolutionQuality = " 4K OLED";
+    television2.TVSize = "50 inches";
+
+    WashingMachine washingMachine1 = new WashingMachine();
+    washingMachine1.Id = 3;
+    washingMachine1.Name = "Bosch";
+    washingMachine1.Description = "Yabancı Çamaşır Makinesi";
+    washingMachine1.ModelYear = 2022;
+    washingMachine1.Price = 8000;
+    washingMachine1.MaxKg = "9 KG";
+    washingMachine1.SpinSpeedRpm = 1400;
+
+    WashingMachine washingMachine2 = new WashingMachine();
+    washingMachine2.Id = 4;
+    washingMachine2.Name = "Arçelik";
+    washingMachine2.Description = "Yerli Çamaşır Makinesi";
+    washingMachine2.ModelYear = 2019;
+    washingMachine2.Price = 6500;
+    washingMachine2.MaxKg = "8 KG";
+    washingMachine2.SpinSpeedRpm = 1200;
+
+    Television[] televisions = { television1, television2 };
+
+    WashingMachine[] washingMachines = { washingMachine1, washingMachine2 };
+
+    for (int i = 0; i < televisions.Length; i++)
+    {
+        Console.WriteLine(televisions[i].Name + " " + televisions[i].Description + " " + televisions[i].ResolutionQuality);
+
+    }
+
+    for (int i = 0; i < washingMachines.Length; i++)
+    {
+        Console.WriteLine(washingMachines[i].Name + " " + washingMachines[i].MaxKg + " " + washingMachines[i].SpinSpeedRpm);
+    }
+}
 Console.WriteLine("--------------Generic Type Örneği--------------");
 
+   static void GenericTypeExample()
+{
+    Box<string> stringKutusu = new Box<string>("Merhaba Dünya");
+    Box<int> tamSayiKutusu = new Box<int>(42);
+
+    Box<string>.DisplayContent(stringKutusu);
+    Box<int>.DisplayContent(tamSayiKutusu);
+}
 
 
+
+//----------------
+
+
+
+
+static void EncapsulationExample()
+{
+    BankAccount bankAccount = new BankAccount("Alice Smith", "123456789", 1000);
+
+    Console.WriteLine($"Customer: {bankAccount.CustomerName}");
+    Console.WriteLine($"Account Number: {bankAccount.AccountNumber}");
+    Console.WriteLine($"Initial Balance: {bankAccount.Balance}");
+
+    bankAccount.Deposit(500);
+    bankAccount.Withdraw(200);
+    bankAccount.Withdraw(2000); // Attempt to withdraw more than the balance
+
+    Console.ReadLine();
+}
+
+static void KapsullemeOrnegi()
+{
+    BankaHesabi bankaHesabi = new BankaHesabi("Tony Stark", "123456789", 1000);
+
+    Console.WriteLine($"Müşteri: {bankaHesabi.MusteriAdi}");
+    Console.WriteLine($"Hesap Numarası: {bankaHesabi.HesapNumarasi}");
+    Console.WriteLine($"Başlangıç Bakiyesi: {bankaHesabi.Bakiye} TL");
+
+    bankaHesabi.ParaYatir(500);
+    bankaHesabi.ParaCek(200);
+    bankaHesabi.ParaCek(2000); // Bakiyeden fazla çekim denemesi
+
+    Console.ReadLine();
+}
 
