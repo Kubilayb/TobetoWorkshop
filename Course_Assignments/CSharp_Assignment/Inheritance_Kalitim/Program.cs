4488bf00c8d714e9f47b731683530b998c4768f1
Kubilayb <109307078+Kubilayb@users.noreply.github.com>
2024-04-01 01:30:23 +0300

    interfaces demo ve inheritance kavramları eklendi.

---
+++ Course_Assignments/CSharp_Assignment/Inheritance_Kalitim/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+// INHERITANCE KALITIM MİRAS
+
+namespace Inheritance_Kalitim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Person[] people = new Person[3]
+            {
+                new Customer{FirstName="Berat"}, 
+                new Student{FirstName="Kubilay"}, 
+                new Person{FirstName="K"}
+            };
+
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Person2
+    {
+        public int Id2 { get; set; }
+    }
+
+    class Customer : Person  // Person2   burada person 2 yi yazamayız bir sınıfı birkez miras aldırabiliriz.İnterface de alabiliriz.
+    {
+        public string City { get; set; }
+    }
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+}
---
+++ Course_Assignments/CSharp_Assignment/InterfacesDemo/Program.cs
@@ -0,0 +1,108 @@
+﻿
+
+namespace InterfacesDemo
+{
+
+
+    //  SRP(Tek Sorumluluk Prensibi) : Bir sınıfın yalnızca bir nedeni olmalıdır değişiklik yapmaya.
+
+    //  OCP(Açık/Kapalı Prensibi): Sınıflar, genişletilmeye açık ancak değişime kapalı olmalıdır.
+
+    //  LSP(Liskov Yerine Geçme Prensibi): Alt sınıflar, üst sınıfların yerine kullanılabilmelidir.
+
+    //  ISP(Arayüz Ayrımı Prensibi): İstemciler, ihtiyaç duymadıkları arayüz metotlarını kullanmamalıdır.
+
+    //  DIP(Bağımlılıkların Ters Çevrilmesi Prensibi): Yüksek seviyeli modüller, düşük seviyeli modüllere bağımlı olmamalıdır.
+    //  Her ikisi de soyutlamalara bağlı olmalıdır.
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+            new Manager(),
+            new Worker(),
+            new Robot()
+            };
+
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+
+            // SOLID prensiplerinden i Harfi => Interface Segregation yaptık.
+
+            IEat[] eats = new IEat[2]
+            {
+                new Worker(),
+                new Manager()
+            };
+        }
+    }
+
+
+    //      ÇOKLU İMPLEMENTASYON
+    interface IWorker
+    {
+        void Work();
+
+    }
+    interface IEat
+    {
+        void Eat();
+    }
+    interface ISalary
+    {
+        void GetSalary();
+
+    }
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Çalışan yemek yedi");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Çalışan maaş aldı");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Çalışıyor");
+        }
+    }
+
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Çalışan yemek yedi");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Çalışan maaş aldı");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Çalışıyor");
+        }
+    }
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("Çalışıyor");
+        }
+    }
+
+}
+
+
