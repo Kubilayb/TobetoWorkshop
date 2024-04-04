a7270a62b4aaad5908c79417bfc472a66b58735b
Kubilayb <109307078+Kubilayb@users.noreply.github.com>
2024-04-05 02:38:04 +0300

    Interface Konusunu Gerçek Hayat Projeleri Mantığı İle Eklendi

---
+++ Course_Assignments/CSharp_Assignment/Interface_Konusu/Program.cs
@@ -0,0 +1,98 @@
+﻿
+
+/*
+class PersonManager
+{
+
+    // implemented operation tamamlanmış operasyon    burada yaptığımız operasyon heryerde ortak demektir
+    public void Add()
+    {
+        Console.WriteLine("Eklendi");
+
+    }
+}
+
+class CustomerManager : PersonManager
+{
+
+}                           BU KODUN İNTEFACE VERSİYONU
+
+*/
+
+
+
+//IPersonManager customerManager = new CustomerManager();
+
+
+//IPersonManager employeeManager = new EmployeeManager();
+
+ProjectManager projectManager = new ProjectManager();
+projectManager.Add(new CustomerManager());
+
+projectManager.Add(new EmployeeManager());
+
+projectManager.Add(new InternManager());    
+
+
+
+// NOT: İNTERFACELER NEWLENEMEZ
+interface IPersonManager
+{
+    // unimplemented operation      Buradaki yaptığımız 
+    void Add(); // imza
+    void Update();
+}
+
+// inherits > class -------   intrerface lerde implements
+
+class CustomerManager : IPersonManager
+{
+    public void Add()
+    {
+        // Müşteri ekleme kodları
+        Console.WriteLine("Müşteri eklendi");
+
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Müşteri güncellendi");
+    }
+}
+
+class EmployeeManager : IPersonManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Personel eklendi");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Personel güncellendi");
+    }
+}
+
+class InternManager : IPersonManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Stajyer eklendi");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Stajtyer güncellendi");
+    }
+}
+
+class ProjectManager
+{
+    public void Add(IPersonManager personManager)
+    {
+        personManager.Add();
+    }
+
+
+}
+
